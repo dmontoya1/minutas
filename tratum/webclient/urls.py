b66069cb829e7abe1f4ce72ace8a69140c574c53
Daniel Montoya <dmontoya@apptitud.com.co>
2018-07-13 18:18:37 -0500

    WIP

--- tratum/webclient/urls.py
+++ tratum/webclient/urls.py
@@ -1,19 +1,16 @@
 
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from .views import (
-    HomePageView,
-    DocumentDetailView,
-    PoliciesView,
-    FAQView,
-    AboutUsView,
-    SignupView
-)
+from . import views
 
 app_name = 'webclient'
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('policies/', PoliciesView.as_view(), name='policies'),
-    path('faq/', FAQView.as_view(), name='faq'),
-    path('about-us/', AboutUsView.as_view(), name='about-us'),
-    path('document/<slug:slug>/', DocumentDetailView.as_view(), name='document'),
+    path('', views.HomePageView.as_view(), name='home'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page="/"), name='logout'),
+    path('signup/', views.SignupView.as_view(), name='signup'),
+    path('policies/', views.PoliciesView.as_view(), name='policies'),
+    path('faq/', views.FAQView.as_view(), name='faq'),
+    path('about-us/', views.AboutUsView.as_view(), name='about-us'),
+    path('document/<slug:slug>/', views.DocumentDetailView.as_view(), name='document'),
 ]
