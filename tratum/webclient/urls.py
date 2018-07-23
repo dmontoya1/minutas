3544b8b6357673a3db5fc395fbaace41b5b0794c
Daniel Felipe Montoya Ramirez <dmontoya@apptitud.com.co>
2018-07-22 22:19:58 -0500

    Updating Profile template

--- tratum/webclient/urls.py
+++ tratum/webclient/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     path('faq/', views.FAQView.as_view(), name='faq'),
     path('about-us/', views.AboutUsView.as_view(), name='about-us'),
     path('document/<slug:slug>/', views.DocumentDetailView.as_view(), name='document'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
 
     #Documents
     url(r'^category/(?P<path>.*)', mptt_urls.view(model='document_manager.models.Category', view='document_manager.views.category', slug_field='slug'), name='category_documents'),
