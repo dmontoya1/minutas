668c831f5ed3e7660f3cf47f78f4f50ca883bece
Daniel Felipe Montoya Ramirez <dmontoya@apptitud.com.co>
2018-08-17 11:15:59 -0500

    Updating alert messages. Changing name of polices, Changing FAQ name, Changing final alert message

--- tratum/tratum/static/js/preview.js
+++ tratum/tratum/static/js/preview.js
@@ -8,7 +8,7 @@ $(function(){
             theme: 'supervan',
             title: '¿Estás seguro que deseas finalizar la edición del documento?',
             content: 'Una vez finalices, no podrás editarlo. Podrás descargar los archivos durante 10 días, \
-            y se te enviará una copia a tu correo electrónico. Pasados los 10 días, el documento caducará.',
+            y se te enviará una copia a tu correo electrónico. Pasados los 10 días, el documento dejará de estar disponible.',
             buttons: {
                 Confirmar: function () {
                     axios.post(`/api/document-manager/finish/`, {identifier: identifier})
