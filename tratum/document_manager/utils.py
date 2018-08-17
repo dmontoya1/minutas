0df0520a7da9d05f395ec7becf54dec918b13d41
Diego Alejandro Castaño Gómez <dcastano@apptitud.com.co>
2018-08-17 08:26:11 -0500

    Hot fix

--- tratum/document_manager/utils.py
+++ tratum/document_manager/utils.py
@@ -3,4 +3,4 @@ def get_static_path(scheme, host, static):
     """Retorna un string formateado con la URL absoluta de un estático o ruta dada.
     """
 
-    return f'{scheme}://{host}{static}'
+    return '{scheme}://{host}{static}'.format(scheme=scheme,host=host,static=static)
