d033f8ffa7506e33bafbe00a9bc5025183fbbb26
Diego Alejandro Castaño Gómez <dcastano@apptitud.com.co>
2018-08-29 17:58:40 -0500

    Hot fix

--- tratum/document_manager/templatetags/fieldformatter.py
+++ tratum/document_manager/templatetags/fieldformatter.py
@@ -2,6 +2,7 @@ from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
 
+
 register = template.Library()
 
 @register.filter(needs_autoescape=True)
@@ -17,7 +18,9 @@ def comma_sep_to_ul(value, autoescape=True):
 @register.filter(needs_autoescape=True)
 @stringfilter
 def comma_sep_to_li(value, autoescape=True):
+    print(value)
     items = value.split('¬')
+    print(items)
     lis = lambda items: [f'<li><p style="text-align:justify">{item}</p></li>' for item in items]
     items = '\n'.join(lis(items))
     return mark_safe(items)
