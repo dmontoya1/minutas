49d658a7b4c03258b563a20b50e7f0adf2f32547
Diego Alejandro Castaño Gómez <dcastano@apptitud.com.co>
2018-08-29 11:40:41 -0500

    Hot fix

--- tratum/tratum/static/js/pdfRender.js
+++ tratum/tratum/static/js/pdfRender.js
@@ -1,12 +1,55 @@
+const units = {
+    1: 'Primer',
+    2: 'Segund',
+    3: 'Tercer',
+    4: 'Cuart',
+    5: 'Quint',
+    6: 'Sext',
+    7: 'Séptim',
+    8: 'Octav',
+    9: 'Noven',
+};
+const tens = {
+    10: 'Décim',
+    20: 'Vigésim',
+    30: 'Trigésim',
+    40: 'Cuadragésim',
+    50: 'Quincuagésim'
+};
 
 (function() {
-    var x = document.querySelectorAll('input[value="dynamic_counter"');
-    var i;
-    for (i = 0; i < x.length; i++) {
-        var span = document.createElement('span');
-        span.innerHTML = i + 1;
-        span.style.color = '#000';
-        x[i].parentNode.replaceChild(span, x[i]);
+    var femaleCounters = document.querySelectorAll('input[value="dynamic_counter"]');
+    var maleCounters = document.querySelectorAll('input[value="dynamic_counter_male"]');
+
+    addCounters(femaleCounters, 'a');
+    addCounters(maleCounters, 'o');
+
+    function addCounters(dynamic_counters, gender){
+        var i;
+        for (i = 0; i < dynamic_counters.length; i++) {
+            i = i + 1
+            if(i.toString().length == 1){
+                counter = units[i] + gender;
+            } else {
+                ten = i.toString()[0] + 0
+                ten = tens[ten]
+                ten = ten + gender
+                unit = i.toString()[1]
+                if(unit == 0){
+                    counter = ten
+                } else {
+                    unit = units[unit] + gender
+                    counter = ten + ' ' + unit
+                }
+            }
+            var span = document.createElement('span');
+            span.style.color = '#000';
+            span.innerHTML = counter.toUpperCase();
+            if(i != 0){
+                i = i - 1;
+            }
+            dynamic_counters[i].parentNode.replaceChild(span, dynamic_counters[i]);
+        }
     }
 
     setTimeout(function(){
@@ -32,5 +75,6 @@
             }
         }
     }, 500);
+
 
 })();
