41c5e7ffbd5f4d20748dcfc59a544c3e08a539d4
Diego Alejandro Castaño Gómez <dcastano@apptitud.com.co>
2018-10-02 16:48:35 -0500

    Hot fix

--- tratum/store/views.py
+++ tratum/store/views.py
@@ -65,20 +65,21 @@ class Checkout(TemplateView):
         taxReturnBase = 0
         description = "Compra realizada desde Tratum"
 
-   
-        test = 1
-        accountId = 512321
-        apiKey = '4Vj8eK4rloUd272L48hsrarnUA'
-        merchantId = 508029
-        url = "https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu"
-        host = 'http://tratum.co'
-     
-        """ test = 0
-        accountId = 746396
-        apiKey = 'vIB29Yn5GW0XVv6qVYBV1e92T1'
-        merchantId = 740818
-        url = "https://checkout.payulatam.com/ppp-web-gateway-payu"
-        host = 'http://tratum.co' """
+
+        if settings.DEBUG:
+            test = 1
+            accountId = 512321
+            apiKey = '4Vj8eK4rloUd272L48hsrarnUA'
+            merchantId = 508029
+            url = "https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu"
+            host = 'http://tratum.apptitud.com'
+        else:
+            test = 0
+            accountId = 746396
+            apiKey = 'vIB29Yn5GW0XVv6qVYBV1e92T1'
+            merchantId = 740818
+            url = "https://checkout.payulatam.com/ppp-web-gateway-payu"
+            host = 'http://tratum.co'
 
         currency = 'COP'
 
