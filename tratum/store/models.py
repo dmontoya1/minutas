9b86b85709bec67c148925580dce5bdb8064fecc
Diego Alejandro Castaño Gómez <dcastano@apptitud.com.co>
2018-10-27 13:55:57 -0500

    Hot fix

--- tratum/store/models.py
+++ tratum/store/models.py
@@ -137,13 +137,10 @@ class UserDocument(models.Model):
         return reverse('webclient:user-document', kwargs={'identifier': self.identifier})
     
     def is_expired(self):
+        diff = datetime.datetime.now() - self.created_at.replace(tzinfo=None)
+        if diff.days > 10:
+            return True
         return False
-        created_at = datetime.datetime.strptime(self.created_at, "%Y-%m-%d") 
-        diff = abs((datetime.datetime.now() - created_at).days)
-        print(diff)
-        if diff > 10:
-            return False
-        return True
 
 
 class Invoice(models.Model):
