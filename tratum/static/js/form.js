b69551b97cba8741a8b8d9d2baf389b1551c78f6
Felipe <felipe@rockalabs.com>
2019-03-26 15:37:35 -0500

    fix issue with grouped fields of number and currency type

--- tratum/static/js/form.js
+++ tratum/static/js/form.js
@@ -321,62 +321,6 @@ function setListeners(){
 
     $('[data-toggle="tooltip"]').tooltip();
 
-    $("#document-form").off('keydown');
-    $("#document-form").on('keydown', 'input', function(e){
-        if(e.which === 9){
-            e.preventDefault();
-        }
-    });
-
-    $("#document-form").off('keyup');
-    $("#document-form").on('keyup', 'input', function(e) {
-        savePreview();
-        e.preventDefault();
-        if($(this).hasClass('pickadate')){
-            return;
-        }
-        if (e.which === 13 || e.which === 9) {
-            if (!($(this).hasClass("required") && $(this).val() == "")){
-                moveNext(this);
-            }
-            return
-        }
-        if (e.which === 38 || e.which === 40) {
-            moveNext(this);
-        }
-    });
-
-    $('#document-form .section-item').off('click');
-    $('#document-form .section-item').on('click', function(e){
-        $(`*[data-section="${$(this).attr('name')}"]`).toggle();
-    })
-
-    $('#document-form .group-adder').off('click');
-    $('#document-form .group-adder').on('click', function(e){
-        e.preventDefault();
-        cloneGroupItem($(this), true);
-    });
-
-    $("#document-form .multiple-checkbox-fields").off('change');
-    $("#document-form .multiple-checkbox-fields").change(function() {
-        var parentName = $(this).data('parent');
-        var parent = $("input[name='"+parentName+"']")
-        $(parent).val("");
-        $(".multiple-checkbox-fields[data-parent='"+parentName+"']").each(function(i, element){
-            var ischecked= $(element).is(':checked');
-            if(ischecked){
-                before = $(parent).val() ;
-                if(before==""){
-                    $(parent).val($(element).val());
-                }else{
-                    $(parent).val( before + "¬" + $(element).val() );
-                }
-            }
-        }).promise().done(function(){
-            savePreview();
-        });
-    });
-
     $('#document-form .date').attr('placeholder', 'Seleccione una fecha...');
 
     $('#document-form .pickadate').pickadate({
@@ -406,9 +350,6 @@ function setListeners(){
         thousandsSeparator: '.',
         centsLimit: 0,
         clearOnEmpty: true,
-         onClose: function() {
-            savePreview();
-        },
     });
 
     $('#document-form .number').priceFormat({
@@ -417,9 +358,65 @@ function setListeners(){
         thousandsSeparator: '.',
         centsLimit: 0,
         clearOnEmpty: false,
-         onClose: function() {
+    });
+
+    $("#document-form").off('keydown');
+    $("#document-form").on('keydown', 'input', function(e){
+        if(e.which === 9){
+            e.preventDefault();
+        }
+    });
+
+
+    $("#document-form").off('keyup');
+    $("#document-form").on('keyup', 'input', function(e) {
+        console.log("keyup");
+        savePreview();
+        e.preventDefault();
+        if($(this).hasClass('pickadate')){
+            return;
+        }
+        if (e.which === 13 || e.which === 9) {
+            if (!($(this).hasClass("required") && $(this).val() == "")){
+                moveNext(this);
+            }
+            return
+        }
+        if (e.which === 38 || e.which === 40) {
+            moveNext(this);
+        }
+    });
+
+    $('#document-form .section-item').off('click');
+    $('#document-form .section-item').on('click', function(e){
+        $(`*[data-section="${$(this).attr('name')}"]`).toggle();
+    })
+
+    $('#document-form .group-adder').off('click');
+    $('#document-form .group-adder').on('click', function(e){
+        e.preventDefault();
+        cloneGroupItem($(this), true);
+    });
+
+    $("#document-form .multiple-checkbox-fields").off('change');
+    $("#document-form .multiple-checkbox-fields").change(function() {
+        console.log("check");
+        var parentName = $(this).data('parent');
+        var parent = $("input[name='"+parentName+"']")
+        $(parent).val("");
+        $(".multiple-checkbox-fields[data-parent='"+parentName+"']").each(function(i, element){
+            var ischecked= $(element).is(':checked');
+            if(ischecked){
+                before = $(parent).val() ;
+                if(before==""){
+                    $(parent).val($(element).val());
+                }else{
+                    $(parent).val( before + "¬" + $(element).val() );
+                }
+            }
+        }).promise().done(function(){
             savePreview();
-        },
+        });
     });
 
     $('#document-form select:not(.dynamic)').off('change');
@@ -433,7 +430,6 @@ function setListeners(){
         loadDynamicFields($(this), e, answers);
     });
 
-    $("#document-form").off('keyup');
     $("#document-form").on('keyup', 'textarea',function (e) {
         savePreview();
     });
@@ -442,7 +438,9 @@ function setListeners(){
 
 }
 
-setListeners();
+$(document).ready(function() {
+    setListeners();
+});
 
 function removeHtmlFieldsRelatedToParent(parent){
     var select = $('[data-question="'+parent+'"]').find('select');
